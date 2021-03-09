b0f8df0c41b653f44c571ac602e3f92f894c22ad
kali <primat.2000@mail.ru>
2021-03-10 02:53:57 +0500

    Bug fixes

--- src/main/resources/static/js/js.js
+++ src/main/resources/static/js/js.js
@@ -3,28 +3,37 @@ function mailSender() {
     let firstname=document.getElementById("firstname").value;
     let lastname=document.getElementById("lastname").value;
     // document.getElementById("button").style.visibility = 'hidden';
-    let data={
-        firstname:firstname,
-        lastname:lastname,
-        email:email
-    }
-    if (validateEmail(email)){
-        document.getElementById("root").innerHTML="<button class=\"buttonload btn btn-success\">\n" +
-            "  <i class=\"fa fa-refresh fa-spin\"></i>Loading\n" +
-            "</button>";
-        axios.post("/api/email",data)
-            .then(function (response) {
 
-                let data=JSON.parse(JSON.stringify(response.data));
-                alert(data.message)
-                console.log(response.data)
-                if (data.success){
-                    window.location.href="/"
-                }
-                else
-                    location.reload()
-            })
-    } else alert("Email noto'g'ri!");
+    if (email.length>0&&firstname.length>0&&lastname.length>0){
+        let data={
+            firstname:firstname,
+            lastname:lastname,
+            email:email
+        }
+        if (validateEmail(email)){
+            document.getElementById("root").innerHTML="<button class=\"buttonload btn btn-success\">\n" +
+                "  <i class=\"fa fa-refresh fa-spin\"></i>Loading\n" +
+                "</button>";
+            axios.post("/api/email",data)
+                .then(function (response) {
+
+                    let data=JSON.parse(JSON.stringify(response.data));
+                    alert(data.message)
+                    console.log(response.data)
+                    if (data.success){
+                        window.location.href="/"
+                    }
+                    else
+                        location.reload()
+                })
+        } else alert("Email noto'g'ri!");
+    }
+    else {
+        alert("Formalarni to'ldiring!")
+        location.reload()
+    }
+
+
 
 }
 
